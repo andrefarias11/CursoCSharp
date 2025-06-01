4b4c74ea95c55fd719b0e81f17d487035ce30ec7
andre farias <andrefarias389@gmail.com>
2025-06-01 13:24:55 -0300

    arrayList

--- array/Arrays/obj/Debug/net9.0/Arrays.AssemblyInfo.cs
+++ array/Arrays/obj/Debug/net9.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7420935eff4f18c96a90e668713660d2e6bee78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
