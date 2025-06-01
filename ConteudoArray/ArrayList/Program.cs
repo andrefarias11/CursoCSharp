ec2a0c7309573a954bac624fdfac68ee77d06bed
andre farias <andrefarias389@gmail.com>
2025-06-01 13:32:07 -0300

    arrayList

---
+++ ConteudoArray/ArrayList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        // Criando um ArrayList
+        ArrayList lista = new ArrayList();
+
+        // Adicionando elementos de diferentes tipos
+        lista.Add(10);
+        lista.Add("Curso C#");
+        lista.Add(3.14);
+        lista.Add(true);
+
+        // Acessando elementos
+        Console.WriteLine("Elementos do ArrayList:");
+        foreach (var item in lista)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Removendo um elemento
+        lista.Remove(3.14);
+
+        // Inserindo em uma posição específica
+        lista.Insert(1, "Novo Elemento");
+
+        Console.WriteLine("\nApós remoção e inserção:");
+        foreach (var item in lista)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Tamanho do ArrayList
+        Console.WriteLine($"\nTamanho do ArrayList: {lista.Count}");
+    }
+}
