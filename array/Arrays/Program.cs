7420935eff4f18c96a90e668713660d2e6bee78d
andre farias <andrefarias389@gmail.com>
2025-06-01 13:20:51 -0300

    Criação e utilização de arrays

---
+++ array/Arrays/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int[] numeros;
+
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes = new string[5] { "João", "Maria", "José", "Ana", "Pedro" };
+
+//Como acessar os elementos do array
+Console.WriteLine(numeros[0]); //João
+Console.WriteLine(nomes[0]); //1
